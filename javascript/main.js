d33303fca7ea50657ad6460a84e9c0b601c555a3
BlueAlien449 <99145495+BlueAlien449@users.noreply.github.com>
2023-04-17 23:12:25 -0600

    Create drawOver function for hover effect and add the required css for showing the trail

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,22 @@
 const grid = document.querySelector('.grid');
 
-
 function makeGrid (rows, cols){
     grid.style.setProperty('--grid-rows', rows);
     grid.style.setProperty('--grid-cols', cols);
     for (i = 0; i < (rows * cols); i++){
         const gridElement = document.createElement('div');
-        grid.appendChild(gridElement).className = 'grid-item'
+        grid.appendChild(gridElement).className = 'grid-item';
+        gridElement.classList.add(i);
     }
     
 }
 
 makeGrid(16,16);
+
+const gridItems = document.querySelectorAll('.grid-item');
+
+function drawOver(e){
+    e.target.classList.add('active');
+}
+
+gridItems.forEach(key => key.addEventListener('mouseover', drawOver));
